e2b67ddfdfba69222f43bcd71a9af31ff641c3af
Kyle Kastner <kastnerkyle@gmail.com>
2020-09-15 07:20:00 +0000

    full sampling with bug fixes

--- examples/transformer_m2xl_music_model/sample_transformer_m2xl_music_model.py
+++ examples/transformer_m2xl_music_model/sample_transformer_m2xl_music_model.py
@@ -208,71 +208,10 @@ targets : 14]
 # this is ... messy but necessary
 
 used_features = [0, 2, 3, 4, 5, 7, 8, 9]
+
 #feature_batches_list = [torch.tensor(b).to(hp.use_device) for n, b in enumerate(batches_list) if n in used_features]
 #feature_batches_masks_list = [torch.tensor(mb).to(hp.use_device) for n, mb in enumerate(batches_masks_list) if n in used_features]
 
-path = valid_files[0]
-pitches, durations, velocities = pitch_duration_velocity_lists_from_music_json_file(path,
-                                                                                    default_velocity=flat_measure_corpus.default_velocity,
-                                                                                    n_voices=flat_measure_corpus.n_voices,
-                                                                                    measure_value=flat_measure_corpus.measure_value,
-                                                                                    fill_value=flat_measure_corpus.fill_value)
-feats = flat_measure_corpus._features_from_lists(pitches, durations, velocities)
-tokens = flat_measure_corpus._tokenize_features(feats)
-
-batches_list = []
-batches_masks_list = []
-# find break points
-break_id = flat_measure_corpus.voices_dictionary.word2idx[9999]
-breaks = np.where(np.array(tokens[5]) == break_id)[0]
-
-for el in range(len(tokens)):
-    tb = np.array(tokens[el])
-    inds = np.arange(len(tb))
-    inds = np.array([i for i in inds if i not in breaks])
-    tb = tb[inds]
-    shp = tb.shape
-    new_tb = tb.repeat(hp.batch_size).reshape(shp + (hp.batch_size,))
-    if len(new_tb.shape) > 2:
-        new_tb = new_tb.transpose(0, 2, 1)
-    else:
-        new_tb = new_tb[..., None]
-    batches_list.append(new_tb)
-    m_new_tb = new_tb[..., 0] * 0
-    m_new_tb[-1] = 1
-    batches_masks_list.append(m_new_tb)
-
-batches_list = [b for n, b in enumerate(batches_list)]
-batches_masks_list = [b for n, b in enumerate(batches_masks_list)]
-feature_batches_list = [torch.tensor(b).to(hp.use_device) for n, b in enumerate(batches_list) if n in used_features]
-feature_batches_masks_list = [torch.tensor(b).to(hp.use_device) for n, b in enumerate(batches_masks_list) if n in used_features]
-
-# AFTER SANITY CHECK, SHIFTED TARGETS BECOME INPUTS TOO
-# 4 sets of targets, alternate definitions of the same value
-# distance from kp0_0 etc
-np_targets = batches_list[-1]
-list_of_inputs = feature_batches_list
-list_of_input_masks = feature_batches_masks_list
-targets = torch.tensor(np_targets).long().to(hp.use_device)
-targets = torch.cat((targets[:1] * 0 + 0, targets), 0)
-# re-adjust to be sure they are the same length
-shifted_targets = targets[:-1]
-targets = targets[1:]
-
-list_of_inputs.extend([shifted_targets[:, :, 0][..., None],
-                       shifted_targets[:, :, 1][..., None],
-                       shifted_targets[:, :, 2][..., None],
-                       shifted_targets[:, :, 3][..., None]])
-
-out0, out1, out2, out3, out_mems = model(list_of_inputs, list_of_input_masks, list_of_mems=None)
-#out0 already cut to [hp.context_len:, :, 0][..., None]
-temperature = args.temperature
-p_cutoff = args.p_cutoff
-reduced = top_p_from_logits_np(out0.cpu().data.numpy() / temperature, p_cutoff)
-reduced_probs = softmax_np(reduced)
-preds = reduced_probs.argmax(axis=-1)
-
-# need to write code to put the audio back together again from given or predictable info
 midi_sample_dir = "midi_samples"
 
 if os.path.exists(midi_sample_dir):
@@ -283,37 +222,168 @@ if os.path.exists(midi_sample_dir):
 if not os.path.exists(midi_sample_dir):
     os.mkdir(midi_sample_dir)
 
-targets2 = copy.deepcopy(targets.cpu().data.numpy())
-preds[:hp.context_len] = targets2[:hp.context_len, :, 0]
+file_indices = list(range(30))
+for fi in file_indices:
+    errored = False
+    random_seed = args.random_seed
+    sampling_random_state = np.random.RandomState(random_seed)
 
-#shuf_i = np.arange(len(preds))
-#np.random.RandomState(2122).shuffle(shuf_i)
-#preds = preds[shuf_i]
+    path = valid_files[fi]
+    pitches, durations, velocities = pitch_duration_velocity_lists_from_music_json_file(path,
+                                                                                        default_velocity=flat_measure_corpus.default_velocity,
+                                                                                        n_voices=flat_measure_corpus.n_voices,
+                                                                                        measure_value=flat_measure_corpus.measure_value,
+                                                                                        fill_value=flat_measure_corpus.fill_value)
+    final_all_pitches_list = None
+    final_all_durations_list = None
+    final_all_voices_list = None
+    final_all_marked_quarters_context_boundary = None
 
-#preds = targets[..., 0]
-#preds = preds[hp.context_len-1:]
+    all_pitch_take_index = 0
+    pitches_indexes = [0, 0, 0, 0]
+    while True:
+        print("Sampling {}, step {}".format(fi, all_pitch_take_index))
+        try:
+            # sometimes predict fingerprint tuples that never existed? might need to handle this in vocabulary
+            feats = flat_measure_corpus._features_from_lists(pitches, durations, velocities)
+            tokens = flat_measure_corpus._tokenize_features(feats)
+            if all_pitch_take_index == 0:
+                print(len(tokens[0]))
+                if len(tokens[0]) > 300:
+                    raise KeyError("")
+        except KeyError:
+            errored = True
+            break
 
-all_pitches_list, all_durations_list, all_voices_list, all_marked_quarters_context_boundary = flat_measure_corpus.pitch_duration_voice_lists_from_preds_and_features(preds, batches_list, batches_masks_list, context_len=hp.context_len)
+        batches_list = []
+        batches_masks_list = []
+        # find break points
+        break_id = flat_measure_corpus.voices_dictionary.word2idx[9999]
+        breaks = np.where(np.array(tokens[5]) == break_id)[0]
 
-for i in range(len(all_pitches_list)):
+        for el in range(len(tokens)):
+            tb = np.array(tokens[el])
+            inds = np.arange(len(tb))
+            inds = np.array([i for i in inds if i not in breaks])
+            tb = tb[inds]
+            shp = tb.shape
+            new_tb = tb.repeat(hp.batch_size).reshape(shp + (hp.batch_size,))
+            if len(new_tb.shape) > 2:
+                new_tb = new_tb.transpose(0, 2, 1)
+            else:
+                new_tb = new_tb[..., None]
+            batches_list.append(new_tb)
+            m_new_tb = new_tb[..., 0] * 0
+            m_new_tb[-1] = 1
+            batches_masks_list.append(m_new_tb)
+
+        batches_list = [b for n, b in enumerate(batches_list)]
+        batches_masks_list = [b for n, b in enumerate(batches_masks_list)]
+        feature_batches_list = [torch.tensor(b).to(hp.use_device) for n, b in enumerate(batches_list) if n in used_features]
+        feature_batches_masks_list = [torch.tensor(b).to(hp.use_device) for n, b in enumerate(batches_masks_list) if n in used_features]
+
+        # AFTER SANITY CHECK, SHIFTED TARGETS BECOME INPUTS TOO
+        # 4 sets of targets, alternate definitions of the same value
+        # distance from kp0_0 etc
+        np_targets = batches_list[-1]
+        list_of_inputs = feature_batches_list
+        list_of_input_masks = feature_batches_masks_list
+        targets = torch.tensor(np_targets).long().to(hp.use_device)
+        targets = torch.cat((targets[:1] * 0 + 0, targets), 0)
+        # re-adjust to be sure they are the same length
+        shifted_targets = targets[:-1]
+        targets = targets[1:]
+
+        list_of_inputs.extend([shifted_targets[:, :, 0][..., None],
+                               shifted_targets[:, :, 1][..., None],
+                               shifted_targets[:, :, 2][..., None],
+                               shifted_targets[:, :, 3][..., None]])
+
+        out0, out1, out2, out3, out_mems = model(list_of_inputs, list_of_input_masks, list_of_mems=None)
+        #out0 already cut to [hp.context_len:, :, 0][..., None]
+        temperature = args.temperature
+        p_cutoff = args.p_cutoff
+        reduced = top_p_from_logits_np(out0.cpu().data.numpy() / temperature, p_cutoff)
+        reduced_probs = softmax_np(reduced)
+        sampled = [[sampling_random_state.choice(np.arange(reduced_probs.shape[-1]), p=reduced_probs[ii, jj]) for jj in range(reduced_probs.shape[1])] for ii in range(len(reduced_probs))]
+        #preds = reduced_probs.argmax(axis=-1)
+        preds = np.array(sampled)
+
+        #shuf_i = np.arange(len(preds))
+        #np.random.RandomState(2122).shuffle(shuf_i)
+        #preds = preds[shuf_i]
+
+        #preds = targets[..., 0]
+        #preds = preds[hp.context_len-1:]
+
+        all_pitches_list, all_durations_list, all_voices_list, all_marked_quarters_context_boundary = flat_measure_corpus.pitch_duration_voice_lists_from_preds_and_features(preds, batches_list, batches_masks_list, context_len=hp.context_len)
+
+        if all_pitch_take_index >= len(all_pitches_list[0]):
+            print("Finished sampling {}".format(path))
+            break
+
+        vv = all_voices_list[0][all_pitch_take_index]
+        pp = all_pitches_list[0][all_pitch_take_index]
+
+        # figure out how to skip keypoints?
+        if durations[vv][pitches_indexes[vv]] == -1:
+            # skip the blanks that denote phrase boundary
+            pitches_indexes[vv] += 1
+
+        if final_all_pitches_list is None:
+            final_all_pitches_list = all_pitches_list
+            final_all_durations_list = all_durations_list
+            final_all_voices_list = all_voices_list
+            final_all_marked_quarters_context_boundary = all_marked_quarters_context_boundary
+
+        marks = all_marked_quarters_context_boundary[0]
+        cur_dur = np.sum([d for d in durations[vv][:pitches_indexes[vv] + 1] if d > 0] + [0])
+        #if cur_dur >= marks[vv]:
+        pitches[vv][pitches_indexes[vv]] = pp
+        final_all_pitches_list[0][all_pitch_take_index] = pp
+
+        pitches_indexes[vv] += 1
+        all_pitch_take_index += 1
+
+        del shifted_targets
+        del targets
+        del out0
+        del out1
+        del out2
+        del out3
+        del out_mems
+
+    i = 0
+    lim = -1
+    if errored:
+        lim = all_pitch_take_index
+        if lim >= 0 and lim < 100:
+            print("SKIPPED {}".format(fi))
+            continue
+
     # handle whatever is giving last entry value of 10000
-    pitches_list = all_pitches_list[i][:-1]
-    durations_list = all_durations_list[i][:-1]
-    voices_list = all_voices_list[i][:-1]
-    marked_quarters_context_boundary = all_marked_quarters_context_boundary[i]
+    pitches_list = final_all_pitches_list[i][:lim]
+    durations_list = final_all_durations_list[i][:lim]
+    voices_list = final_all_voices_list[i][:lim]
+    marked_quarters_context_boundary = final_all_marked_quarters_context_boundary[i]
 
     data = convert_voice_lists_to_music_json(pitch_lists=pitches_list, duration_lists=durations_list, voices_list=voices_list)
     tstamp = int(time.time())
-    json_fpath = midi_sample_dir + os.sep + "true{}_{}.json".format(i, tstamp)
+    json_fpath = midi_sample_dir + os.sep + "sampled{}_{}.json".format(fi, tstamp)
     write_music_json(data, json_fpath)
 
-    fpath = midi_sample_dir + os.sep + "true{}_{}.midi".format(i, tstamp)
+    fpath = midi_sample_dir + os.sep + "sampled{}_{}.midi".format(fi, tstamp)
     a = "harpsichord_preset"
     b = "woodwind_preset"
     m = {0: [(a, 0), (b, marked_quarters_context_boundary[0])],
          1: [(a, 0), (b, marked_quarters_context_boundary[1])],
          2: [(a, 0), (b, marked_quarters_context_boundary[2])],
          3: [(a, 0), (b, marked_quarters_context_boundary[3])]}
+
+    a = "harpsichord_preset"
+    m = {0: [(a, 0),],
+         1: [(a, 0),],
+         2: [(a, 0),],
+         3: [(a, 0),]}
     music_json_to_midi(data, fpath, voice_program_map=m)
     print("Wrote out {}".format(fpath))
-from IPython import embed; embed(); raise ValueError()
